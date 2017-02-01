2251f4a16869b77edb22a7a34ad58859d1afa4bc
Jeremy Biffis <Jeremy Biffis>
2017-02-01 15:37:57 -0500

    Mirror the folder structure of the raid drive

--- Scripts/transferToBackup.bat
+++ Scripts/transferToBackup.bat
@@ -1,4 +1,12 @@
-SET src="E:\ForBackup"
-SET dst="\\mediabox\m"
 
-robocopy %src% %dst% /e /mov
+SET src=E:\ForBackup
+SET dst=\\mediabox\m
+
+
+robocopy %src% %dst% /s /move   &:: Move files to the server
+if not exist %src%\NUL mkdir %src%
+robocopy %src% %src% /s /move   &:: Delete empty directories - This should leave us with a blank slate Unless there were errors copying a file
+if not exist %src%\NUL mkdir %src%
+attrib -s -h -r %src%
+robocopy %dst% %src% /e /xf * /xd $RECYCLE.BIN "System Volume Information" OneDriveTemp /R:0 /LEV:3  &:: Replace all directories on local system
+attrib -s -h -r %src%
