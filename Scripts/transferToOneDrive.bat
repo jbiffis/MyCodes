6f5585e6307256d32b4716c43ddbfdb361bb16b7
Jeremy Biffis <Jeremy Biffis>
2017-02-10 00:21:02 -0500

    change paths
     fixes issue id:1 #1

--- Scripts/transferToOneDrive.bat
+++ Scripts/transferToOneDrive.bat
@@ -1,3 +1,3 @@
-node transferToOneDrive.js src="M:\Temp Photo Landing Zone\MaddyCameraRoll" dest=M:\OneDrive\Pictures\Photos
-node transferToOneDrive.js src="M:\Temp Photo Landing Zone\JeremyCameraRoll" dest=M:\OneDrive\Pictures\Photos
-node transferToOneDrive.js src="M:\Temp Photo Landing Zone\OtherPhotos" dest=M:\OneDrive\Pictures\Photos
+node NodeScripts\transferToOneDrive.js src="M:\Temp Photo Landing Zone\MaddyCameraRoll" dest=M:\OneDrive\Pictures\Photos
+node NodeScripts\transferToOneDrive.js src="M:\Temp Photo Landing Zone\JeremyCameraRoll" dest=M:\OneDrive\Pictures\Photos
+node NodeScripts\transferToOneDrive.js src="M:\Temp Photo Landing Zone\OtherPhotos" dest=M:\OneDrive\Pictures\Photos
